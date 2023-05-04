93e64e1d1f899b92ef5171f483bff7833c728c5f
madhavjani <madhavjani1996@gmail.com>
2023-05-04 00:10:21 -0300

    Additional

--- Extract.py
+++ Extract.py
@@ -16,19 +16,20 @@
 
 import os
 import pandas as pd
-
+import Transform
 # Set the path to the directory containing the files
-path = '/users/madhavjani/PycharmProjects/DataEngineer/Stocks/etfs'
+etfs_path = '/users/madhavjani/PycharmProjects/DataEngineer/Stocks/etfs'
+stock_path = '/users/madhavjani/PycharmProjects/DataEngineer/Stocks/etfs'
 
 # Get a list of all files in the directory
-file_list = os.listdir(path)
+file_list = os.listdir(etfs_path)
 
 # Create an empty list to store the dataframes
 df_list = []
 
 # Loop through each file and load it into a dataframe
 for file_name in file_list:
-    file_path = os.path.join(path, file_name)
+    file_path = os.path.join(etfs_path, file_name)
     if os.path.isfile(file_path):
         # Load the file into a dataframe
         df = pd.read_csv(file_path)
---
+++ Transform.py
@@ -0,0 +1,25 @@
+from pyspark.sql import SQLContext
+from pyspark.sql import Functions as F
+import pyspark.sql.types as T
+from pyspark.sql.functions import *
+from pyspark.sql.window import Window
+
+class Transform:
+
+    def symbols_valid_meta(self,symbols_valid_meta):
+
+        compute_df=0
+
+        return compute_df
+
+    def stocks(self,stocks):
+
+        compute_stocks=0
+
+        return compute_stocks
+
+    def etfs(self,etfs):
+
+        computw_etfs=0
+
+        return computw_etfs
